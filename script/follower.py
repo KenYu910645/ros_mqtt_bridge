2c8d5aa2357e8d1215581b376bb1084c06b0e03b
Your Name <you@example.com>
2020-07-05 19:34:40 +0800

    change broker ip

--- script/follower.py
+++ script/follower.py
@@ -9,7 +9,7 @@ def main(args):
     rospy.init_node('ros_mqtt_bridge', anonymous=False)
     
     # init ros_mqtt_bridge
-    ros_mqtt_bridge = Ros_mqtt_bridge(client_id="mqtt_follower", broker_ip="broker.hivemq.com", port=1883, keepalive=10, clean_session=True)
+    ros_mqtt_bridge = Ros_mqtt_bridge(client_id="mqtt_follower", broker_ip="10.0.0.1", port=1883, keepalive=10, clean_session=True)
     # ros_mqtt_bridge init publisher
     # ros_mqtt_bridge.init_publisher(ros_topic = "ros_cmd_master", mqtt_topic = "mqtt_cmd", data_type = "geometry_msgs/Twist")
     ros_mqtt_bridge.init_tf_subscriber(frame_id = "map", child_id = "base_link_peer",mqtt_topic="mqtt_base_link")    
--- script/leader.py
+++ script/leader.py
@@ -11,7 +11,7 @@ def main(args):
     #----- Init node ------# 
     rospy.init_node('ros_mqtt_bridge', anonymous=False)
     # init ros_mqtt_bridge
-    ros_mqtt_bridge = Ros_mqtt_bridge(client_id="mqtt_leader", broker_ip="broker.hivemq.com", port=1883, keepalive=10, clean_session=True)
+    ros_mqtt_bridge = Ros_mqtt_bridge(client_id="mqtt_leader", broker_ip="10.0.0.1", port=1883, keepalive=10, clean_session=True)
 
     r = rospy.Rate(10) #call at 50HZ # Need to be faster than tf hz 
     while (not rospy.is_shutdown()):
